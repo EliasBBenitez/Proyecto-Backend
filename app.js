fc3306a4075ad563bee39adf48f6dad6b10f5273
EliasBenitez <eliasb.benitez@gmail.com>
2023-05-23 18:51:43 -0300

    tercer entrega

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const ProductManager = require('./ProductManager');
+
+const app = express();
+const productManager = new ProductManager('products.json');
+
+app.get('/products', async (req, res) => {
+  try {
+    const limit = req.query.limit ? parseInt(req.query.limit) : undefined;
+    const products = await productManager.getProducts();
+    const response = limit ? products.slice(0, limit) : products;
+    res.json(response);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.get('/products/:pid', async (req, res) => {
+  try {
+    const productId = parseInt(req.params.pid);
+    const product = await productManager.getProductById(productId);
+    if (!product) {
+      res.status(404).json({ error: 'Product not found' });
+    } else {
+      res.json(product);
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
